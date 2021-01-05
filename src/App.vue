364067dbd8b80b50f43650a9bb47b11044a04c03
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2021-01-05 08:34:58 -0300

    feat: component app styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
   <div id="app">
-    <Conversor moeda-a="USD" moeda-b="BRL"/>
+    <div class="block">
+      <Conversor moeda-a="USD" moeda-b="BRL" />
+      <Conversor moeda-a="BRL" moeda-b="USD" />
+    </div>
+
+    <div class="block">
+      <Conversor moeda-a="CAD" moeda-b="BRL" />
+      <Conversor moeda-a="BRL" moeda-b="USD" />
+    </div>
+
+    <div class="block">
+      <Conversor moeda-a="BRL" moeda-b="BRL" />
+      <Conversor moeda-a="HKD" moeda-b="USD" />
+    </div>
+
+    <div class="block">
+      <Conversor moeda-a="BRL" moeda-b="BRL" />
+      <Conversor moeda-a="INR" moeda-b="USD" />
+    </div>
   </div>
 </template>
 
 <script>
-import Conversor from "./components/Conversor.vue";
-export default {
-  name: 'App',
-  components: {
-    Conversor
+  import Conversor from "./components/Conversor.vue";
+  export default {
+    name: 'App',
+    components: {
+      Conversor
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    max-width: 900px;
+    margin: 0 auto;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .block {
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -2,7 +2,7 @@
   <div class="conversor">
     <h2>{{moedaA}} To {{moedaB}}</h2>
     <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
-    <input type="button" value="Converter">
+    <input type="button" value="Converter" v-on:click="converter">
     <h2>{{moedaB_value}}</h2>
   </div>
 </template>
@@ -11,30 +11,35 @@
 <script>
   export default {
     name: "Conversor",
-    props: ["moedaA","moedaB"],
+    props: ["moedaA", "moedaB"],
     data() {
       return {
-        moedaA_value : "",
-        moedaB_value : 0,
-      }
+        moedaA_value: "",
+        moedaB_value: 0
+      };
     },
     methods: {
       converter() {
-        let de_para = this.moedaA + "_" + this.moedaB;
-        let url = "http://free.currencyconverterapi.com/api/v5/convert?q=" +
-          de_para
-        + "&compact=y";
-
-        fetch(url).then(res => {return res.json}).then(json => {
-          let cotacao = json[de_para].val;
-          this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
-        })
+        let de_para = this.moedaA + '_' + this.moedaB;
+        let url = 'https://free.currencyconverterapi.com/api/v6/convert?q=' + de_para + '&compact=y&apiKey=9efaf120b22fde4b9a53';
+        fetch(url)
+          .then(res => {
+            return res.json()
+          })
+          .then(json => {
+            let cotacao = json[de_para].val;
+            this.moedaB_value = (parseFloat(this.moedaA_value) * cotacao).toFixed(2)
+          })
       }
     }
-  }
+  };
 </script>
 
 
-<style scoped>
-
+<style scoped >
+  .conversor {
+    padding: 20px;
+    max-width: 300px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  }
 </style>
