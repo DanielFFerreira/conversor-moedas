33f0417e534c375a17a9fab4627252af73c21b6b
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2021-01-04 22:08:21 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Conversor from "./components/Conversor.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Conversor
   }
 }
 </script>
---
+++ src/components/Conversor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="conversor">
+    <h2>{{moedaA}} To {{moedaB}}</h2>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: "Conversor",
+    props: ["moedaA","moedaB"]
+  }
+</script>
+
+
+<style scoped>
+
+</style>
